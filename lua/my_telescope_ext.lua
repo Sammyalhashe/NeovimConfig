72a86f94c3e676c1d37dc0378a900568f1495728
Sammy Al Hashemi <salhashemi2@bloomberg.net>
2022-09-22 11:04:19 -0400

    Making live_grep smarter to ignore worktrees.

--- lua/my_telescope_ext.lua
+++ lua/my_telescope_ext.lua
@@ -1,10 +1,34 @@
-local builtin = require"telescope.builtin"
+local builtin = require "telescope.builtin"
+local Job = require "plenary.job"
+local utils = require "utils"
 local M = {}
 
 function M.find_sah_marks()
     builtin.grep_string({
         prompt_title = "Stuff Sammy Changed",
         search = "@sah -- start",
+    })
+end
+
+function M.git_files_grep_symbol()
+    local ignored_directories = {}
+    Job:new({
+        "git", "worktree", "list",
+        on_stdout = function(_, data)
+
+            for worktree in data:gmatch("%S+") do
+                print(worktree)
+                local split_string = utils.split_string(worktree, "/")
+                local size = #split_string
+                local escaped_string, _ =
+                split_string[size]:gsub("%[", ""):gsub("%]", "")
+                table.insert(ignored_directories, "!" .. escaped_string)
+            end
+        end
+    }):sync()
+
+    builtin.live_grep({
+        glob_pattern = ignored_directories
     })
 end
 
--- lua/utils.lua
+++ lua/utils.lua
@@ -1,21 +1,21 @@
 local M = {}
 
 function M.is_buffer_empty()
-  -- Check whether the current buffer is empty
-  return vim.fn.empty(vim.fn.expand('%:t')) == 1
+    -- Check whether the current buffer is empty
+    return vim.fn.empty(vim.fn.expand('%:t')) == 1
 end
 
 function M.has_width_gt(cols)
-  -- Check if the windows width is greater than a given number of columns
-  return vim.fn.winwidth(0) / 2 > cols
+    -- Check if the windows width is greater than a given number of columns
+    return vim.fn.winwidth(0) / 2 > cols
 end
 
 function M.map(type, key, value)
-    vim.api.nvim_buf_set_keymap(0,type,key,value,{noremap = true, silent = true});
+    vim.api.nvim_buf_set_keymap(0, type, key, value, { noremap = true, silent = true });
 end
 
 function M.map_allbuf(type, key, value)
-    vim.api.nvim_set_keymap(type,key,value,{noremap = true, silent = true});
+    vim.api.nvim_set_keymap(type, key, value, { noremap = true, silent = true });
 end
 
 function M.printTable(table)
@@ -24,5 +24,15 @@ function M.printTable(table)
     end
 end
 
+function M.split_string(inputstr, sep)
+    if sep == nil then
+        sep = "%s"
+    end
+    local t = {}
+    for str in string.gmatch(inputstr, "([^" .. sep .. "]+)") do
+        table.insert(t, str)
+    end
+    return t
+end
 
 return M
