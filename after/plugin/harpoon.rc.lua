cf584d3ac57cc2966c2de5257127ea05c1b115ec
salhashemi2 <salhashemi2@bloomberg.net>
2024-11-21 18:23:04 -0500

    Harpoon keybinding changes.

--- after/plugin/harpoon.rc.lua
+++ after/plugin/harpoon.rc.lua
@@ -12,8 +12,8 @@ utils.map_allbuf("n", "<leader>ha", ":lua require('harpoon.mark').add_file()<CR>
 --> toggle harpoon menu
 utils.map_allbuf("n", "<leader>ht", ":lua require('harpoon.ui').toggle_quick_menu()<CR>")
 --> previous/next harpoon toggle
-utils.map_allbuf("n", "<leader>h[", ":lua require('harpoon.ui').nav_prev()<CR>")
-utils.map_allbuf("n", "<leader>h]", ":lua require('harpoon.ui').nav_next()<CR>")
+utils.map_allbuf("n", "<leader>hp", ":lua require('harpoon.ui').nav_prev()<CR>")
+utils.map_allbuf("n", "<leader>hn", ":lua require('harpoon.ui').nav_next()<CR>")
 
 --> cmd-ui
 utils.map_allbuf("n", "<leader>hc", ":lua require('harpoon.cmd-ui').toggle_quick_menu()<CR>")
